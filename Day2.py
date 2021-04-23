5d4730054a5d9415c85bb7790b2d5714636efeee
Ramya-Buddala <82963514+Ramya-Buddala@users.noreply.github.com>
2021-04-23 19:32:07 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,6 @@
+name = "Haier"
+quantity = 276
+rating = 3
+Type = "Double Door"
+waranty = 7
+print("Name of the refrigerator is: {}.\nQuantity is : {} litres with {} star rating of {} type.\nWaranty is {} years".format(name,quantity,rating,Type,waranty))
